3fc42a4c94d7c5ba9b5b7be7b15f2817c0c61888
tbhesswebber <tbhesswebber@gmail.com>
2018-10-08 10:27:07 -0400

    adds comments to romanNumerals

--- romanNumerals.js
+++ romanNumerals.js
@@ -30,16 +30,22 @@ const arabicToRoman = {
 };
 
 export function parseArabic(arabic = 0) {
+  /**
+   * TODO: Look for ways to optimize... nestedLoops === NOBUENO
+   *    Maybe cache values on the server or memoize?
+   */
   let [roman, current] = ['', arabic];
   const pairs = Object.entries(arabicToRoman);
   while (current > 0) {
     // use a for loop for the break statement;
     for (let i = 0; i < pairs.length; i += 1) {
       const [currentArabic, currentRoman] = pairs[i];
+      // when the current arabic is 1000, we are at the top of the known number system - aka infinity.
       const nextArabic = pairs[i + 1] ? pairs[i + 1][0] : Infinity;
       if (+currentArabic <= current && +nextArabic > current) {
         roman += currentRoman;
         current -= currentArabic;
+        // want to update current within our while loop rather than continue comparing against values that fail the condition
         break;
       }
     }
@@ -47,8 +53,8 @@ export function parseArabic(arabic = 0) {
   return roman;
 }
 
-// ! this implementation will parse ANY roman numeral, not just valid ones.
 export function parseRoman(roman = '') {
+  // ! this implementation will parse ANY roman numeral, not just valid ones.
   return roman.split('').reduce((arabic, numeral, index, fullRoman) => {
     const currentValue = romanToArabic[numeral];
     const nextNumeral = fullRoman[index + 1];
